380c088a1c9cd6666d7d2207dd6041f8a10da93f
leandromac <contato@leandromacedo.com>
2020-09-27 10:56:50 -0300

    Creating application markup

--- src/app.js
+++ src/app.js
@@ -1,19 +1,52 @@
 'use strict'
 
 import React from 'react'
-import Title from './title'
 
-var App = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <Title
-          name='Leandro Macedo'
-          age='34'
-        />
+const App = () => (
+  <div className='app'>
+
+    <div className='search'>
+      <input
+        type='search'
+        placeholder='Digite o nome de usuário do Github'
+      />
+    </div>
+
+    <div className='user-info'>
+      <img src='https://avatars2.githubusercontent.com/u/4989860?v=4' />
+      <h1>
+        <a href='https://github.com/leandromac'>Leandro Macedo</a>
+      </h1>
+
+      <div className='repos-info'>
+        <ul>
+          <li>Repositórios: 100</li>
+          <li>Seguidores: 10</li>
+          <li>Seguindo: 10</li>
+        </ul>
       </div>
-    )
-  }
-})
+
+      <div className='actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
+      </div>
+
+      <div className='repos'>
+        <h2>Repositórios:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+)
 
 export default App
